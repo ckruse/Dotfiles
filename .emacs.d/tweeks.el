fad98ca70f45d66cce9ff22d36f0248ef9e931e4
Christian Kruse <cjk@wwwtech.de>
2011-01-27 12:45:18 +0100

    some smaller tweeks for org-mode and mobile-org

--- .emacs.d/tweeks.el
+++ .emacs.d/tweeks.el
@@ -80,9 +80,9 @@
 
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 
-(setq org-directory "~/org")
+(setq org-directory "~/Dropbox/org")
 (setq org-mobile-directory "~/Dropbox/MobileOrg/")
-(setq org-agenda-files (quote ("~/Dropbox/org/projects.org")))
+(setq org-agenda-files (quote ("~/Dropbox/org/projects.org" "~/Dropbox/org/inbox.org")))
 (setq org-mobile-inbox-for-pull "~/Dropbox/org/inbox.org")
 
 (add-hook 'org-mode-hook 'turn-on-font-lock)
